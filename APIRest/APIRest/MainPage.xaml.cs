89513f20455b1a58374fe5646edce06f4c484a55
Idílio Casimiro <idiliocasimiro@outlook.com>
2018-11-25 13:43:12 +0100

    Add project files.

---
+++ APIRest/APIRest/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace APIRest
+{
+    public partial class MainPage : ContentPage
+    {
+        private string Url = "https://jsonplaceholder.typicode.com/posts";
+        ObservableCollection<Post> posts;
+        //1 - Criar o client
+        private HttpClient client = new HttpClient();
+
+        public MainPage()
+        {    
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            
+            //2 - Obter o conteúdo em forma de string
+            var conteudo = await client.GetStringAsync(Url);
+            //3 - Deserializar a string
+            var listaPosts = JsonConvert.DeserializeObject<List<Post>>(conteudo);
+            posts = new ObservableCollection<Post>(listaPosts);
+
+            ListaPosts.ItemsSource = posts;
+
+            base.OnAppearing();
+        }
+
+        private async void Add(object sender, EventArgs e)
+        {
+            var post = new Post() { Title = "Idílio", Body = "Exemplo de Post" + DateTime.Now.Ticks };
+            var json_post = JsonConvert.SerializeObject(post);
+            await client.PostAsync(Url, new StringContent(json_post));
+
+            posts.Insert(0, post);
+        }
+
+        private async void Update(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Delete(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Post
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
